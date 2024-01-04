a3814af448200295e70f646d563dcacafa4b5535
Siddharth Mishra-Sharma <smsharma@mit.edu>
2024-01-04 17:25:11 -0500

    Draft SSL nb

---
+++ multimodal_ssl/models/transformer_utils.py
@@ -0,0 +1,138 @@
+import torch
+import torch.nn as nn
+from torch.nn import functional as F
+
+
+class SelfAttention(nn.Module):
+    """
+    Canonical implementation of multi-head self attention.
+    """
+
+    def __init__(self, emb, heads=2):
+        """
+        :param emb:
+        :param heads:
+        """
+
+        super().__init__()
+
+        assert emb % heads == 0, f"Embedding dimension ({emb}) should be divisible by nr. of heads ({heads})"
+
+        self.emb = emb
+        self.heads = heads
+
+        # We will break the embedding into `heads` chunks and feed each to a different attention head
+
+        self.tokeys = nn.Linear(emb, emb, bias=False)
+        self.toqueries = nn.Linear(emb, emb, bias=False)
+        self.tovalues = nn.Linear(emb, emb, bias=False)
+
+        self.unifyheads = nn.Linear(emb, emb)
+
+    def forward(self, x, mask=None):
+        b, t, e = x.size()
+        h = self.heads
+        assert e == self.emb, f"Input embedding dim ({e}) should match layer embedding dim ({self.emb})"
+
+        s = e // h
+
+        keys = self.tokeys(x)
+        queries = self.toqueries(x)
+        values = self.tovalues(x)
+
+        keys = keys.view(b, t, h, s)
+        queries = queries.view(b, t, h, s)
+        values = values.view(b, t, h, s)
+
+        # -- We first compute the k/q/v's on the whole embedding vectors, and then split into the different heads.
+        #    See the following video for an explanation: https://youtu.be/KmAISyVvE1Y
+
+        # Compute scaled dot-product self-attention
+
+        # - fold heads into the batch dimension
+        keys = keys.transpose(1, 2).contiguous().view(b * h, t, s)
+        queries = queries.transpose(1, 2).contiguous().view(b * h, t, s)
+        values = values.transpose(1, 2).contiguous().view(b * h, t, s)
+
+        queries = queries / (e ** (1 / 4))
+        keys = keys / (e ** (1 / 4))
+        # - Instead of dividing the dot products by sqrt(e), we scale the keys and values.
+        #   This should be more memory efficient
+
+        # - get dot product of queries and keys, and scale
+        dot = torch.bmm(queries, keys.transpose(1, 2))
+
+        assert dot.size() == (b * h, t, t)
+
+        if mask is not None:
+            mask = mask.unsqueeze(1).unsqueeze(2)  # mask is initially (b, t). We need to make it (b, 1, 1, t)
+            mask = mask.expand(b, h, t, t)  # Now expand mask to (b, h, t, t) to cover all heads and sequence length
+            mask = mask.reshape(b * h, t, t)  # Reshape to (b * h, t, t) to match the dot product tensor
+            dot = dot.masked_fill_(~mask, float("-inf"))
+
+        dot = F.softmax(dot, dim=2)
+        # - dot now has row-wise self-attention probabilities
+
+        # apply the self attention to the values
+        out = torch.bmm(dot, values).view(b, h, t, s)
+
+        # swap h, t back, unify heads
+        out = out.transpose(1, 2).contiguous().view(b, t, s * h)
+
+        return self.unifyheads(out)
+
+
+class TransformerBlock(nn.Module):
+    def __init__(self, emb, heads, ff_hidden_mult=6, dropout=0.0):
+        super().__init__()
+
+        self.attention = SelfAttention(emb, heads=heads)
+
+        self.norm1 = nn.LayerNorm(emb)
+        self.norm2 = nn.LayerNorm(emb)
+
+        self.ff = nn.Sequential(nn.Linear(emb, ff_hidden_mult * emb), nn.ReLU(), nn.Linear(ff_hidden_mult * emb, emb))
+
+        self.do = nn.Dropout(dropout)
+
+    def forward(self, x, mask=None):
+        attended = self.attention(x, mask=mask)
+        x = self.norm1(attended + x)
+        x = self.do(x)
+        fedforward = self.ff(x)
+        x = self.norm2(fedforward + x)
+        x = self.do(x)
+        return x
+
+
+class Transformer(nn.Module):
+    """
+    Transformer for classifying sequences
+    """
+
+    def __init__(self, emb, heads, depth, dropout=0.0):
+        """
+        :param emb: Embedding dimension
+        :param heads: nr. of attention heads
+        :param depth: Number of transformer blocks
+        :param seq_length: Expected maximum sequence length
+        :param num_classes: Number of classes.
+        :param max_pool: If true, use global max pooling in the last layer. If false, use global
+                         average pooling.
+        """
+        super().__init__()
+
+        self.tblocks = nn.ModuleList([TransformerBlock(emb=emb, heads=heads, dropout=dropout) for _ in range(depth)])
+        self.do = nn.Dropout(dropout)
+
+    def forward(self, x, mask=None):
+        """
+        :param x: A batch by sequence length integer tensor of token indices.
+        :return: predicted log-probability vectors for each token based on the preceding tokens.
+        """
+
+        x = self.do(x)
+        for tblock in self.tblocks:
+            x = tblock(x, mask)
+
+        return x
--- simulation-based-inference/README.md
+++
@@ -1,6 +0,0 @@
-# IAIFI Astro Hackathon: Tutorial on Simulation-Based Inference
-
-Tentative plan:
-- Notebook adapted from the MIT tutorials, covering implicit likelihoods, NRE, NPE, ABC, Coverage
-- Notebook using Quijote data and GNNs
-- Hackathon ideas: MMD-based mismodeling adaptation, applying to own simulator, improving Quijote-based SBI as you choose
