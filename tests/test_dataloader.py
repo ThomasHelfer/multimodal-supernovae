75e8dea33b901d7e6598652b314d13a996811707
thelfer1 <thomashelfer@live.de>
2024-02-28 11:40:58 -0500

    added more tests

--- tests/test_dataloader.py
+++ tests/test_dataloader.py
@@ -12,9 +12,23 @@ from src.dataloader import (
 
 
 def test_dataloader():
-    # Loading 
+    # Loading spectra and galaxy 
     max_data_len = 1000
-    dataset, nband = load_data("ZTFBTS", "ZTFBTS_spectra/", max_data_len)
+    load_galaxy = True
+    # Note that load_data has internal asserts, checking if the right files are mached 
+    dataset, nband = load_data("ZTFBTS", "ZTFBTS_spectra/", max_data_len,load_galaxy)
+    assert(next(iter(dataset)) == 5)
+    assert(nband == 1)
+    # Loading lightcurve and galaxy 
+    max_data_len = 1000
+    dataset, nband = load_data("ZTFBTS", None, max_data_len)
+    assert(next(iter(dataset)) == 5)
+    assert(nband == 2)
+    host_imgs, mag, time, mask, magerr = next(iter(dataset))
+    for t,m in time,mask:
+        print(torch.min(t[mask]))
+        assert(torch.min(t[mask]) ==0)
+
 
 
 if __name__ == "__main__":
