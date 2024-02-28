e37750828ca0815dc47a04c1e872fdff1d5cfb8d
thelfer1 <thomashelfer@live.de>
2024-02-28 11:53:16 -0500

    fixed test for dataloader

--- tests/test_dataloader.py
+++ tests/test_dataloader.py
@@ -17,12 +17,12 @@ def test_dataloader():
     load_galaxy = True
     # Note that load_data has internal asserts, checking if the right files are mached 
     dataset, nband = load_data("ZTFBTS", "ZTFBTS_spectra/", max_data_len,load_galaxy)
-    assert(next(iter(dataset)) == 5)
+    assert(len(next(iter(dataset))) == 5)
     assert(nband == 1)
     # Loading lightcurve and galaxy 
     max_data_len = 1000
     dataset, nband = load_data("ZTFBTS", None, max_data_len)
-    assert(next(iter(dataset)) == 5)
+    assert(len(next(iter(dataset))) == 5)
     assert(nband == 2)
     host_imgs, mag, time, mask, magerr = next(iter(dataset))
     for t,m in time,mask:
