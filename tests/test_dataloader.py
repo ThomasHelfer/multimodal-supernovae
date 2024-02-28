8d636eff10a7e6473e9ee540e62bea40651e9dd7
thelfer1 <thomashelfer@live.de>
2024-02-28 11:07:48 -0500

    added first test

--- tests/test_dataloader.py
+++ tests/test_dataloader.py
@@ -1,10 +1,20 @@
 import torch
 import os
 import sys
+from src.dataloader import (
+    load_images,
+    load_lightcurves,
+    load_spectras,
+    load_data,
+    plot_lightcurve_and_images,
+    NoisyDataLoader,
+)
 
 
 def test_dataloader():
-    pass
+    # Loading 
+    max_data_len = 1000
+    dataset, nband = load_data("ZTFBTS", "ZTFBTS_spectra/", max_data_len)
 
 
 if __name__ == "__main__":
