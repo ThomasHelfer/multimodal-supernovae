1c63abdae8add0570a71488d3d3a3045135e980a
thelfer1 <thomashelfer@live.de>
2024-06-15 10:41:54 -0400

    fixed unit-tests

--- tests/test_dataloader.py
+++ tests/test_dataloader.py
@@ -12,29 +12,45 @@ from src.dataloader import (
 
 
 def test_dataloader():
-    # Loading spectra and galaxy 
+    # Loading spectra and galaxy
     max_data_len = 100
-    # Note that load_data has internal asserts, checking if the right files are mached 
-    dataset, nband,_ = load_data("ZTFBTS", "ZTFBTS_spectra/", max_data_len_spec=max_data_len, combinations=["spectral", 'host_galaxy'])
-    assert(len(next(iter(dataset))) == 7)
-    assert(nband == 1)
-    # Loading lightcurve and galaxy 
+    # Note that load_data has internal asserts, checking if the right files are mached
+    dataset, nband, _, _ = load_data(
+        "ZTFBTS",
+        "ZTFBTS_spectra/",
+        max_data_len_spec=max_data_len,
+        combinations=["spectral", "host_galaxy"],
+    )
+    assert len(next(iter(dataset))) == 7
+    assert nband == 1
+    # Loading lightcurve and galaxy
     max_data_len = 100
-    dataset, nband,_ = load_data("ZTFBTS", None, max_data_len_lc=max_data_len, combinations=["lightcurve", 'host_galaxy'])
-    assert(len(next(iter(dataset))) == 7)
-    assert(nband == 2)
+    dataset, nband, _, _ = load_data(
+        "ZTFBTS",
+        None,
+        max_data_len_lc=max_data_len,
+        combinations=["lightcurve", "host_galaxy"],
+    )
+    assert len(next(iter(dataset))) == 7
+    assert nband == 2
 
     # Checking that time indeed starts at 0 for all lightcurves
     iterator = iter(dataset)
     for i in range(len(dataset)):
         host_imgs, mag, time, mask, magerr, z, classification = next(iterator)
-        assert(torch.min(time[mask])==0)
+        assert torch.min(time[mask]) == 0
 
     # Loading lightcurve and spectra
     max_data_len = 100
-    dataset, nband,_ = load_data("ZTFBTS", "ZTFBTS_spectra/", max_data_len_lc=max_data_len, combinations=['host_galaxy', "lightcurve", 'spectral'])
-    assert(len(next(iter(dataset))) == 11)
-    assert(nband == 2)
+    dataset, nband, _, _ = load_data(
+        "ZTFBTS",
+        "ZTFBTS_spectra/",
+        max_data_len_lc=max_data_len,
+        combinations=["host_galaxy", "lightcurve", "spectral"],
+    )
+    assert len(next(iter(dataset))) == 11
+    assert nband == 2
+
 
 if __name__ == "__main__":
     test_dataloader()
